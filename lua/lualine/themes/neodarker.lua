95ef6688290e1654871480a2ebaa98a6a4ee791b
Naman Kumar <naman@outlook.in>
2023-03-31 12:34:55 +0530

    Update neodarker.lua

--- lua/lualine/themes/neodarker.lua
+++ lua/lualine/themes/neodarker.lua
@@ -6,38 +6,38 @@ local lualine = {}
 
 lualine.normal = {
   a = { bg = palette.blue, fg = palette.black },
-  b = { bg = palette.blue_2, fg = palette.blue },
-  c = { bg = palette.blue_2, fg = palette.fg },
+  b = { bg = palette.bg, fg = palette.blue },
+  c = { bg = palette.bg, fg = palette.fg },
 }
 
 lualine.insert = {
   a = { bg = palette.green, fg = palette.black },
-  b = { bg = palette.blue_2, fg = palette.green },
-  c = { bg = palette.blue_2, fg = palette.fg },
+  b = { bg = palette.bg, fg = palette.green },
+  c = { bg = palette.bg, fg = palette.fg },
 }
 
 lualine.command = {
   a = { bg = palette.yellow, fg = palette.black },
-  b = { bg = palette.blue_2, fg = palette.yellow },
-  c = { bg = palette.blue_2, fg = palette.fg },
+  b = { bg = palette.bg, fg = palette.yellow },
+  c = { bg = palette.bg, fg = palette.fg },
 }
 
 lualine.visual = {
   a = { bg = palette.purple, fg = palette.black },
-  b = { bg = palette.blue_2, fg = palette.purple },
-  c = { bg = palette.blue_2, fg = palette.fg },
+  b = { bg = palette.bg, fg = palette.purple },
+  c = { bg = palette.bg, fg = palette.fg },
 }
 
 lualine.replace = {
   a = { bg = palette.red, fg = palette.black },
-  b = { bg = palette.blue_2, fg = palette.red },
-  c = { bg = palette.blue_2, fg = palette.fg },
+  b = { bg = palette.bg, fg = palette.red },
+  c = { bg = palette.bg, fg = palette.fg },
 }
 
 lualine.inactive = {
-  a = { bg = palette.blue_2, fg = palette.blue },
-  b = { bg = palette.blue_2, fg = palette.fg },
-  c = { bg = palette.blue_2, fg = palette.fg },
+  a = { bg = palette.bg, fg = palette.blue },
+  b = { bg = palette.bg, fg = palette.fg },
+  c = { bg = palette.bg, fg = palette.fg },
 }
 
 return lualine
